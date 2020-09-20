6a5e62c586d69cc526fb8a7375957dc5143ada25
liujiahao <edinliu@gmail.com>
2020-09-20 08:03:11 +0800

    新增readme

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const line = require('@line/bot-sdk');
 const client = new line.Client({
   // 這有兩個官方帳號
   // try20200919
-  channelAccessToken: 'l8uzmsSt16fkqrLuisPq0RSgP0ANrmQk2r2QnwllzwCCYnwiz1KXAr2BbUzgvrZmXlChADTifxIdHpraWfGs3lxAQIhf0EFqxwEAf14xMSyBWdQYBI7PwcOMZJ8JhcU7kgfnAsgCX77kLs2OENN+qgdB04t89/1O/w1cDnyilFU='
+  // channelAccessToken: 'l8uzmsSt16fkqrLuisPq0RSgP0ANrmQk2r2QnwllzwCCYnwiz1KXAr2BbUzgvrZmXlChADTifxIdHpraWfGs3lxAQIhf0EFqxwEAf14xMSyBWdQYBI7PwcOMZJ8JhcU7kgfnAsgCX77kLs2OENN+qgdB04t89/1O/w1cDnyilFU='
   // test123 
-  // channelAccessToken: 'O+k0c0NaBbllEuRrErSXDkG4UloemoO97iEWK0EMr2qciHnGbv4ysKiQXQYk0O8HU8yCj4BAhjDGrJWK0rLePJew2i44lU7HwGMIn3sSxI7z7THEhxr5RgD+kTMD47XGiDiO0xvo/cCQ/fjSY9W4bAdB04t89/1O/w1cDnyilFU='
+  channelAccessToken: 'O+k0c0NaBbllEuRrErSXDkG4UloemoO97iEWK0EMr2qciHnGbv4ysKiQXQYk0O8HU8yCj4BAhjDGrJWK0rLePJew2i44lU7HwGMIn3sSxI7z7THEhxr5RgD+kTMD47XGiDiO0xvo/cCQ/fjSY9W4bAdB04t89/1O/w1cDnyilFU='
 });
 
 const uidList = [
-  // 這有兩個好友
-  'U12cf5a7d53acb513bbe5498aeb1b46a5',
-  'Ud3c61dc859a83313398bb1192c0e65ed',
+  // 這是我的LINE的 user id，上面兩個官方帳號都有加入
+  'U12cf5a7d53acb513bbe5498aeb1b46a5',// 必須知道加入官方帳號的好友的user id才能對他發訊息
+  // 'jiahao1111' // 一般用來加LINE好友的ID不能用
 ]
 const message = {
   type: 'text',
-  text: 'hello'
+  text: 'AAAAAAAAAAAAA'
 };
 client.multicast(uidList, message)
   .then((res) => {
---
+++ readme.md
@@ -0,0 +1,8 @@
+這個程式可以對加入 LINE 官方帳號並且知道他的 USER ID 的好友發訊息
+
+# 使用方法
+
+1. 到 LINE 官方帳號管理頁面複製 channelAccessToken，貼在 index.js 中
+2. 把好友的 user id 加入至 index.js 的 uidList 中
+3. \$ node index.js
+4. 訊息會傳到我的LINE
